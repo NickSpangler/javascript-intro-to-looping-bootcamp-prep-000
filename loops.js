7a80237878755737713b212d1f62ae86dff6d2c8
Nick Spangler <spangleyman@mac.com>
2020-05-14 20:34:14 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+var strangeLoops = []
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+   array.push(`I am ${i} strange loop.`); 
+    }
+   else {
+     array.push(`I am ${i} strange loops.`)
+  }
+ }
+ return array;
+}
+forLoop(strangeLoops);
+
